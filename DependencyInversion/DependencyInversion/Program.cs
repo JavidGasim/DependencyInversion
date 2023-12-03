5545add31d5d01165606faabf338b62df4c4d972
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-12-03 17:04:25 +0400

    Add files via upload

---
+++ DependencyInversion/DependencyInversion/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace DependencyInversion;
+
+public interface IMessageSender
+{
+    void SendMessage(string message);
+}
+
+public class EmailSender : IMessageSender
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine($"Sending email: {message}");
+    }
+}
+
+public class SmsSender : IMessageSender
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine($"Sending SMS: {message}");
+    }
+}
+
+public class NotificationService
+{
+    private IMessageSender _messageSender;
+
+    public NotificationService(IMessageSender messageSender)
+    {
+        _messageSender = messageSender;
+    }
+
+    public void NotifyUser(string username)
+    {
+
+        _messageSender.SendMessage($"Hello {username}, you have a new notification.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IMessageSender emailSender = new EmailSender();
+        IMessageSender smsSender = new SmsSender();
+
+        NotificationService emailNotificationService = new NotificationService(emailSender);
+        NotificationService smsNotificationService = new NotificationService(smsSender);
+
+        emailNotificationService.NotifyUser("Javid Gasim");
+        smsNotificationService.NotifyUser("Mehemmed Nugedi");
+    }
+}
